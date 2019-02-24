0462eff688c847f2b33e755416e3e2e418f5630a
jinpyojeon <jinpyo.jeon@trincoll.edu>
2019-02-23 22:02:52 -0500

    fixed indent issue

--- proj_1.py
+++ proj_1.py
@@ -2,15 +2,16 @@ import sys
 import random
 import heapq
 
+
 class Puzzle:
-    
+
     LEFT = 0
     RIGHT = 1
     UP = 2
     DOWN = 3
 
-    parity_0 = [1,2,3,4,5,6,7,8,0]
-    parity_1 = [1,2,3,8,0,4,7,6,5]
+    parity_0 = [1, 2, 3, 4, 5, 6, 7, 8, 0]
+    parity_1 = [1, 2, 3, 8, 0, 4, 7, 6, 5]
 
     def __init__(self, input_arr=None):
         if input_arr:
@@ -19,10 +20,10 @@ class Puzzle:
             self.state = [i for i in range(9)]
             random.shuffle(self.state)
             self.goal_state = self.get_goal_state()
-        
+
     def print_state(self, state):
         print('{0}\n{1}\n{2}'.format(
-            ' '.join([str(state[i]) for i in range(0, 3)]), 
+            ' '.join([str(state[i]) for i in range(0, 3)]),
             ' '.join([str(state[i]) for i in range(3, 6)]),
             ' '.join([str(state[i]) for i in range(6, 9)])))
 
@@ -57,7 +58,7 @@ class Puzzle:
         last_action = None
         if len(actions) > 0:
             last_action = actions[-1]
-        
+
         add_new_action = lambda x: possible_actions.append(actions + [x])
 
         if i - 3 >= 0 and last_action != Puzzle.DOWN:
@@ -70,17 +71,17 @@ class Puzzle:
             add_new_action(Puzzle.DOWN)
 
         return possible_actions
-    
+
     def find_empty_space(self, state):
         return state.index(0)
 
     def get_updated_state(self, actions, state=None):
         current_state = list(state) if state else list(self.state)
         empty = self.find_empty_space(state if state else self.state)
-    
+
         def swap(i):
             current_state[i], current_state[empty] = current_state[empty], current_state[i]
-            return i 
+            return i
 
         for a in actions:
             if a == Puzzle.UP:
@@ -91,26 +92,26 @@ class Puzzle:
                 empty = swap(empty - 1)
             if a == Puzzle.RIGHT:
                 empty = swap(empty + 1)
-        
+
         return current_state
 
     def serialize_actions(self, actions):
         return ''.join([str(a) for a in actions])
 
     def bfs_search(self):
-        
-        queue = [] 
+
+        queue = []
         queue.append([])
 
         tried_actions = {}
 
-        action_str = lambda x : ''.join([str(a) for a in x])
+        action_str = lambda x: ''.join([str(a) for a in x])
 
         node_count = 1
 
         while len(queue) > 0:
             p = queue.pop(0)
-            
+
             '''
             self.print_state(self.state)
             self.print_actions(p)
@@ -118,14 +119,14 @@ class Puzzle:
             '''
 
             if self.get_updated_state(p) == self.goal_state:
-                return self.state, p, node_count 
-            
-            tried_actions[action_str(p)] = 1            
+                return self.state, p, node_count
+
+            tried_actions[action_str(p)] = 1
             print(len(action_str(p)))
 
             for a in self.get_possible_actions(p):
 
-                if action_str(a) not in tried_actions:                
+                if action_str(a) not in tried_actions:
                     queue.append(a)
                     node_count += 1
 
@@ -134,9 +135,9 @@ class Puzzle:
 
         tried_actions = {}
 
-        action_str = lambda x : ''.join([str(a) for a in x])
-        
-        cost = lambda a: len(a) +  h(self.get_updated_state(a), self.goal_state)
+        action_str = lambda x: ''.join([str(a) for a in x])
+
+        cost = lambda a: len(a) + h(self.get_updated_state(a), self.goal_state)
 
         heapq.heappush(pqueue, (cost([]), []))
 
@@ -144,10 +145,10 @@ class Puzzle:
 
         while len(pqueue) > 0:
             c, p = heapq.heappop(pqueue)
-            
+
             if self.get_updated_state(p) == self.goal_state:
                 return self.state, p, node_count
-        
+
             tried_actions[action_str(p)] = 1
 
             print(c - len(p))
@@ -156,30 +157,31 @@ class Puzzle:
             # self.print_state(self.goal_state)
 
             for a in self.get_possible_actions(p):
-            
+
                 if action_str(a) not in tried_actions:
                     heapq.heappush(pqueue, (cost(a), a))
                     node_count += 1
 
 
 def manhattan(curr_state, goal_state):
-	assert len(curr_state) == 9 and len(goal_state) == 9
-	total_diff = 0
-	for i, v in enumerate(curr_state):
-		goal_i = goal_state.index(v)
-		vert_diff = abs(goal_i - i) // 3
-		hori_diff = abs(goal_i - i) % 3
-		total_diff += vert_diff + hori_diff
-	return total_diff  
+    assert len(curr_state) == 9 and len(goal_state) == 9
+    total_diff = 0
+    for i, v in enumerate(curr_state):
+        goal_i = goal_state.index(v)
+        vert_diff = abs(goal_i - i) // 3
+        hori_diff = abs(goal_i - i) % 3
+        total_diff += vert_diff + hori_diff
+    return total_diff
+
 
 if __name__ == '__main__':
-	print(sys.argv)   
-	# if len(sys.argv) == 10:
-	# 	input_arr = [i for i in sys.argv[2:]]
-	
-	puzzle = Puzzle(input_arr) if input_arr else Puzzle()
+    input_arr = None
+    if len(sys.argv) == 10:
+        input_arr = [i for i in sys.argv[1:]]
 
-    # state, actions, count = puzzle.bfs_search() 
+    puzzle = Puzzle(input_arr) if input_arr else Puzzle()
+
+    # state, actions, count = puzzle.bfs_search()
     state, actions, count = puzzle.a_star_search(manhattan)
 
     puzzle.print_state(puzzle.get_updated_state(actions, state))
